6aeb8a8fe1dffd01ef98f520f05372f4a1f7face
DaniDRGo <danielnddrago@gmail.com>
2022-10-31 08:53:01 -0300

    se maqueto home y se hizo responsive

--- views/apiProductos.js
+++ views/apiProductos.js
@@ -5,6 +5,9 @@ const URL ='https://api.thecatapi.com/v1/images/search'
 fetch(URL)
  .then(res =>res.json())
  .then(data =>{
-    const img = document.querySelector('img')
-    img.src = data[0].url
+    const img = document.querySelectorAll("img[class='imagen-productos-home']")
+    for(let i = 0;i < img.length; i++){
+     img.src = data[0].url
+    }
+    
  })
